928a9a42bd5e033594b5cebe77e8a8379d2455df
capt-ines <ines.swieczkowska02@gmail.com>
2025-03-27 15:19:21 +0100

    feat: test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
+  const bbebb: any = 100;
   return (
     <Html lang="en">
       <Head />
+      {bbebb}
       <body className="antialiased">
         <Main />
         <NextScript />
