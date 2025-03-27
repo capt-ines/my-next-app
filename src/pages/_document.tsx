609ae48448aa27a7a0d148536333fbaf1a288886
capt-ines <ines.swieczkowska02@gmail.com>
2025-03-27 15:45:22 +0100

    feat: test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const bbebb: any = "stringgg";
+  const bbebb: any = "strinnnnnnggg";
   return (
     <Html lang="en">
       <Head />
