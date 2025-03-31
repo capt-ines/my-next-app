bf33eac68d959c01a174614a6daaa19c60e3cd4d
capt-ines <ines.swieczkowska02@gmail.com>
2025-03-31 15:49:52 +0200

    test: added new workflow file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  const bbebb: any = "strinnnnnnggg";
   return (
     <Html lang="en">
       <Head />
-      {bbebb}
       <body className="antialiased">
         <Main />
         <NextScript />
