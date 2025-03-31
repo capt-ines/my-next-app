9334f0fc22117d43f90375043889e7d4316c9040
capt-ines <ines.swieczkowska02@gmail.com>
2025-03-31 22:51:06 +0200

    test: changed github actions file

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,8 +11,6 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const testVar: number = "90";
-
 export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
