235e443cbc2ef6ac368104158da65976dc0b067f
capt-ines <ines.swieczkowska02@gmail.com>
2025-04-11 16:57:30 +0200

    fix: layout animation bug

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -10,7 +10,7 @@ interface LayoutProps {
 function Layout({ children }: LayoutProps) {
   return (
     <div className="bg-global-background/30 min-h-screen min-w-screen overflow-hidden">
-      <div className="absolute -z-50 h-screen w-screen overflow-hidden blur-3xl">
+      <div className="absolute -z-40 h-screen w-screen overflow-hidden blur-3xl">
         <Particle center={{ x: 100, y: 100 }} />
         <Particle center={{ x: 100, y: 100 }} />
         <Particle center={{ x: 800, y: 400 }} />
